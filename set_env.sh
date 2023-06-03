a1018db6822960b1f072e589fa87f371bbb66f2b
Ngo Kha <>
2023-06-03 17:58:57 +0700

    initial

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=khanq1
+export POSTGRES_PASSWORD=12345678
+export POSTGRES_HOST=p3cdr.c0cbrtzapgxy.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=p3-939009092585-dev
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
